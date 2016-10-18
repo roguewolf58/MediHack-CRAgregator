03387a1c29897cf8b91f51c019b8ea72647e3400
Bryan Hensarling <bryan.hensarling@gmail.com>
2016-10-17 19:55:59 -0500

    UI changes

---
+++ CRAgregator/CRAgregator.Web/App/dataService.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    'use strict';
+
+    var serviceId = 'dataService';
+    angular.module('app').factory(serviceId, ['$http', 'common', dataService]);
+
+    function dataService($http, common) {
+        var $q = common.$q;
+
+        var data = {
+            SearchText: ''
+        };
+
+        var service = {
+            getSearchText: function () { return data.SearchText; },
+            setSearchText: function (searchText) { data.SearchText = searchText; },
+            getData: getData
+        };
+
+        return service;
+
+        // Employee
+        function getData() {
+            var url = 'API/api/Employee/Employees/';
+            var req = $http.get(url);
+            return req;
+
+        }
+
+        function getTestData() {
+            var people = [
+                { Sponsor: 'Sponsor1', Study: 'Study1', Site: 'Site1', OpenQueries: 5, AnsweredQueries: 6 },
+                { Sponsor: 'Sponsor2', Study: 'Study1', Site: 'Site1', OpenQueries: 5, AnsweredQueries: 6 },
+                { Sponsor: 'Sponsor2', Study: 'Study1', Site: 'Site1', OpenQueries: 5, AnsweredQueries: 6 },
+                { Sponsor: 'Sponsor2', Study: 'Study1', Site: 'Site1', OpenQueries: 5, AnsweredQueries: 6 },
+                { Sponsor: 'Sponsor3', Study: 'Study1', Site: 'Site1', OpenQueries: 5, AnsweredQueries: 6 },
+                { Sponsor: 'Sponsor3', Study: 'Study1', Site: 'Site1', OpenQueries: 5, AnsweredQueries: 6 },
+                { Sponsor: 'Sponsor4', Study: 'Study1', Site: 'Site1', OpenQueries: 5, AnsweredQueries: 6 }
+            ];
+            return $q.when(people);
+        }
+
+    }
+})();
---
+++ CRAgregator/CRAgregator.Web/App/mainPage.js
@@ -0,0 +1,49 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'mainPageList';
+    angular.module('app').controller(controllerId, ["$http", "$q", "$window", "$state", 'dataService', mainPageList]);
+
+    function ProgramList($http, $q, $window, $state, common, clinicalAdminService) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.title = 'Programs';
+        vm.rowCollection = [];
+        vm.defaultSortType = function () {
+            vm.sortType = 'Name';
+            vm.sortReverse = false;
+        };
+        vm.defaultSortType();
+
+        vm.selected = {};
+        vm.showError = false;
+        vm.errorMessage = "";
+        vm.formClick = function () {
+            vm.showError = false;
+        };
+
+        vm.loading = false;
+
+        activate();
+
+        function activate() {
+            var promises = [getRowCollection()];
+            common.activateController(promises, controllerId)
+                .then(function () { log('Activated Main View'); });
+        }
+
+        function getRowCollection() {
+            vm.loading = true;
+            var promiseGet = dataService.getTestData();
+            return promiseGet.then(function (result) {
+                vm.rowCollection = result.data;
+                vm.loading = false;
+                return $q.when(vm.rowCollection);
+            }, function (err) {
+                log('An Error Occurred. See more details at the top of the form.');
+            });
+        }
+    }
+
+})();
